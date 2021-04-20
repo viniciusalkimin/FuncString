a69bc6ee4bd78bb5e153505ef43504bfcadbdac0
Vinicius Alkimin <71612344+viniciusalkimin@users.noreply.github.com>
2021-04-20 20:37:06 -0300

    finalversion

---
+++ FuncStringVet.java
@@ -0,0 +1,18 @@
+
+public class FuncStringVet {
+	public static void main(String[]args) {
+		
+		String s = "Teclado Mouse Monitor";
+		
+		String[] vect = s.split(" ");
+		
+		System.out.println(vect[0]);
+		System.out.println(vect[1]);
+		System.out.println(vect[2]);
+		
+		System.out.println(s);
+		
+		
+	}
+
+}
